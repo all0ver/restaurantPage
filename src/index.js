7c8b30c16d8a328e04617de9c15ddca2675e4fbe
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-24 02:40:01 +0000

    menu fix, stopPropagation() - js

--- src/index.js
+++ src/index.js
@@ -9,58 +9,7 @@ import { menu } from "./modules/menu.js";
 
 const container = document.querySelector("#content");
 
-// main page on start;
-main(container);
-const addMenu = () => {
-  const extra = document.querySelector(".menuExtra");
-  extra.addEventListener("click", () => {
-    changeAndClose();
-    menu(container);
-  })
-}
-addMenu();
-
-const mainBtn = document.querySelector("#home");
-const menuBtns = document.querySelectorAll(".menu");
-const aboutBtn = document.querySelector("#about");
-
-const changeAndClose = () => {
-  if (container.firstChild) {
-    if (container.firstChild) {
-      container.innerHTML = "";
-    }
-    if (document.body.style.overflow === "hidden") {
-      toggleMenu();
-    }
-  }
-}
-
-mainBtn.addEventListener("click", () => {
-  changeAndClose();
-  main(container);
-  addMenu();
-})
-
-//menuBtns.addEventListener("click", () => {
-//  changeAndClose();
-//  menu(container);
-//})
-
-aboutBtn.addEventListener("click", () => {
-  changeAndClose();
-  about(container);
-})
-
-menuBtns.forEach((element) => {
-  element.addEventListener("click", () => {
-    changeAndClose();
-    menu(container);
-  })
-})
-
 // hamburger menu
-
-
 
 const menuBtn = document.querySelector(".menuBtn");
 const navUl = document.querySelector(".navUl");
@@ -73,9 +22,50 @@ const toggleMenu = () => {
   }
   navUl.classList.toggle("invisible");
   navUl.classList.toggle("menuShow");
+  console.log("togglemenu");
+  event.stopPropagation();
 }
 
 menuBtn.addEventListener("click", toggleMenu);
 navUl.addEventListener("click", toggleMenu);
 
 
+// main div content
+const mainBtn = document.querySelector("#home");
+const menuBtns = document.querySelectorAll(".menu");
+const aboutBtn = document.querySelector("#about");
+
+const changeAndClose = () => {
+  if (container.firstChild) {
+      container.innerHTML = "";
+  }
+  toggleMenu();
+}
+
+mainBtn.addEventListener("click", () => {
+  changeAndClose();
+  main(container);
+  addMenu();
+})
+
+aboutBtn.addEventListener("click", () => {
+  changeAndClose();
+  about(container);
+})
+
+menuBtns.forEach((element) => {
+  element.addEventListener("click", () => {
+    changeAndClose();
+    menu(container);
+  })
+})
+// main page on start;
+main(container);
+const addMenu = () => {
+  const extra = document.querySelector(".menuExtra");
+  extra.addEventListener("click", () => {
+    changeAndClose();
+    menu(container);
+  })
+}
+addMenu();
