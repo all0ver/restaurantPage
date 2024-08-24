d34b41c3bbfe21cb58e190b2689ace10086a5731
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-24 02:48:57 +0000

    prepare to deploy

--- src/modules/about.js
+++
@@ -1,5 +0,0 @@
-export const about = (container) => {
-  const element = document.createElement("p");
-  element.textContent = "about";
-  container.appendChild(element);
-}
--- src/modules/menu.js
+++
@@ -1,5 +0,0 @@
-export const menu = (container) => {
-  const element = document.createElement("p");
-  element.textContent = "menu";
-  container.appendChild(element);
-}
